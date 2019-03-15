b2b90ae6c5963297980acd6606e9485ef9657d8d
cristian2005 <36666070+cristian2005@users.noreply.github.com>
2019-03-15 12:54:52 -0400

    lasijdaklsd
    lkasdkljaljskdlaksd

--- application/views/Pagina_Principal.php
+++ application/views/Pagina_Principal.php
@@ -10,7 +10,7 @@ $this->load->view('Menu/Inicio/Login');
 <?php
 $this->load->view('Menu/Inicio/Anuncios');
 ?>
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
+    <button type="button" class="btn btn-primary asd" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
 
 <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm">
